8b2e391cb61d3bbec4560ae3622dbcf285cf3ecb
Toshiyuki Yoshida <yostos@yostos.org>
2024-03-07 16:31:41 +0900

    Add Python and AWS related gitignore and Python script

---
+++ awsfig.py
@@ -0,0 +1,67 @@
+## AWS のアーキテクチャー図から CDKのTypeScript定義を生成する
+
+import boto3
+import json
+import base64
+import sys
+import os
+import click 
+
+
+# コマンドライン引数の設定
+@click.command()
+@click.option('--debug','-D',is_flag=True, help='Debug mode')
+@click.option('--file', '-f',prompt='AWS Architecture Image Filename',help='Input Image Filename')
+
+def generate(debug,file):
+
+    """This tool generate Typescript from AWS figure"""
+
+    image = open(file, "rb").read()
+    b64 = base64.b64encode(image).decode("utf-8")
+
+    # 指定可能なリージョンはバージニア北部（us-east-1）またはオレゴン（us-west-2）
+    # デフォルトリージョンで良い場合はリージョン指定省略可
+    # サンプルのため認証情報が直接書き込まれています。適切な方法で取得するようにしてください。
+    bedrock = boto3.client('bedrock-runtime', 
+                        aws_access_key_id = 'to be pasted',
+                        aws_secret_access_key = 'to be pasted',
+                        region_name = "us-west-2")
+
+
+    body = json.dumps(
+        {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 1000,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": "image/png",
+                            "data": b64
+                        }
+                    },
+                    {
+                        "type": "text",
+                        "text": "画像の構成を実現するAWS CDKのコードをTypeScriptで書いて提示してください。"
+                    }
+                ]
+                }
+            ]
+        }
+    )
+    modelId = 'anthropic.claude-3-sonnet-20240229-v1:0'
+    accept = 'application/json'
+    contentType = 'application/json'
+    response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    response_body = json.loads(response.get('body').read())
+    answer = response_body["content"][0]["text"]
+    print(answer)
+
+
+if __name__ == '__main__':
+    generate()
